52ec9a72e984682bf57a9e720b5e35db103e7ce2
admin-kac <41816441+admin-kac@users.noreply.github.com>
2018-10-28 13:52:53 +0530

    initial
    initial

---
+++ gatsby-config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    plugins: [
+      {
+        resolve: 'gatsby-source-storyblok',
+        options: {
+          accessToken: 'gTQKOuqQ0uROv5Hpsr87fwtt',
+          homeSlug: 'home',
+          version: 'draft'
+        }
+      }
+    ]
+  }
